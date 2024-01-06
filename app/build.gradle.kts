46bb4d7cac1a8b84b4b5b2bfb370b04363140024
Ammar1602 <115075702+Ammar1602@users.noreply.github.com>
2024-01-06 16:40:28 +0700

    add class TicketOrderActivity and TicketOrderViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,8 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation("androidx.compose.material3:material3:1.2.0-beta01")
 
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
--- app/src/main/java/com/example/bioskop1/TicketOrderActivity.kt
+++ app/src/main/java/com/example/bioskop1/TicketOrderActivity.kt
@@ -1,2 +1,38 @@
 package com.example.bioskop1
 
+import android.os.Build
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.example.bioskop1.api.ApiRetrofit
+import com.example.bioskop1.model.Seat
+import com.example.bioskop1.ui.theme.Bioskop1Theme
+
+class TicketOrderActivity : ComponentActivity(){
+    private  val api by lazy { ApiRetrofit().apiEndPoint }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(
+        savedInstanceState: Bundle?,
+        persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Bioskop1Theme {
+
+            }
+        }
+    }
+}
+
+class TicketOrderViewModel {
+    var selectedTime by mutableStateOf("Select time schedule")
+    var selectedDate by mutableStateOf("Open date picker dialog")
+    var selectedSeats by mutableStateOf<Set<Seat>>(seatOf())
+
+}

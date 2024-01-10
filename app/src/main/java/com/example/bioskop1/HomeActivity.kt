53690a7483baebacce695adcff36bf12d8fe602e
Ammar1602 <115075702+Ammar1602@users.noreply.github.com>
2024-01-10 14:42:37 +0700

    add fun TopAppBar

--- app/src/main/java/com/example/bioskop1/HomeActivity.kt
+++ app/src/main/java/com/example/bioskop1/HomeActivity.kt
@@ -8,9 +8,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
@@ -26,6 +28,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.bioskop1.api.ApiRetrofit
@@ -65,6 +70,41 @@ class HomeActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun CustomTopBar(){
+    val configuration = LocalConfiguration.current
+    val screenWidthDp = configuration.screenWidthDp
+    val isWideScreen = screenWidthDp.dp > 600.dp
+
+    val topBarHeight = if (isWideScreen)118.dp else 50.dp
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(topBarHeight)
+            .background(
+                color = MaterialTheme.colorScheme.primary,
+                shape = RoundedCornerShape(
+                    bottomStart = 30.dp,
+                    bottomEnd = 30.dp
+                )
+            ),
+        contentAlignment = Alignment.TopCenter
+    ){
+        Text(
+            text = "Movie List",
+            style = MaterialTheme.typography.headlineMedium.copy(
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            ),
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .fillMaxSize(),
+            textAlign = TextAlign.Center
+        )
     }
 }
 

7246b9cbaa0971fce72956dc69d214bb6108a8f3
Ammar1602 <115075702+Ammar1602@users.noreply.github.com>
2024-01-10 14:15:27 +0700

    add fun TicketGrid

--- app/src/main/java/com/example/bioskop1/HomeActivity.kt
+++ app/src/main/java/com/example/bioskop1/HomeActivity.kt
@@ -1,33 +1,73 @@
 package com.example.bioskop1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.bioskop1.api.ApiRetrofit
+import com.example.bioskop1.model.Ticket
 import com.example.bioskop1.ui.theme.Bioskop1Theme
 
 class HomeActivity : ComponentActivity() {
+    private val api by lazy { ApiRetrofit().apiEndPoint }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Bioskop1Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                ){
+                    MovieGrid()
+
+                    CustomTopBar()
+
+                    Button(
+                        onClick = {
+                            val intent = Intent(this@HomeActivity,
+                                MyTicketActivity::class.java)
+                            this@HomeActivity.startActivity(intent)
+                        },
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .padding(vertical = 8.dp, horizontal = 16.dp)
+                            .fillMaxWidth(),
+                        colors =  ButtonDefaults.buttonColors(containerColor = Color.Black)
+                    ) {
+                        Text(text = " My Ticket", color = Color.White)
+                    }
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
--- app/src/main/java/com/example/bioskop1/MyTicketActivity.kt
+++ app/src/main/java/com/example/bioskop1/MyTicketActivity.kt
@@ -3,13 +3,24 @@ package com.example.bioskop1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.bioskop1.model.Ticket
 import com.example.bioskop1.ui.theme.Bioskop1Theme
 
 class MyTicketActivity : ComponentActivity() {
@@ -17,17 +28,38 @@ class MyTicketActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Bioskop1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting3("Android")
+                Column {
+                    CustomTopBarr()
+                    TicketGrid()
                 }
             }
         }
     }
+
+    @Composable
+    private fun  TicketGrid(){
+        var ticket by remember { mutableStateOf<List<Ticket>> (emptyList()) }
+
+        LaunchedEffect(key1 = Unit){
+            getTicket()?.let{
+                ticket = it
+            }
+        }
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(1),
+            modifier = Modifier.padding(16.dp)
+        ){
+            items(ticket){
+                TicketCard(ticket = ticket)
+            }
+        }
+    }
+
+    private suspend fun getTicket():
 }
+
+
 
 @Composable
 fun Greeting3(name: String, modifier: Modifier = Modifier) {
